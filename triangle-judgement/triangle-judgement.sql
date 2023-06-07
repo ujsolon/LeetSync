c02eb9c2e20cdb73b02a98cb1846fd0acde06599
ujsolon <60507757+ujsolon@users.noreply.github.com>
2023-06-07 10:56:29 +0800

    Time: 679 ms (9.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ triangle-judgement/triangle-judgement.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT x, y, z,
+  CASE WHEN x+y<= z THEN 'No'
+   WHEN y+z<= x THEN 'No'
+   WHEN x+z<= y THEN 'No'
+  ELSE 'Yes'
+  END as triangle #
+FROM Triangle
