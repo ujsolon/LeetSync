dde52cbac4b2ec948989d6f3f2700e204bf30f3c
ujsolon <60507757+ujsolon@users.noreply.github.com>
2023-06-09 20:41:36 +0800

    Time: 1102 ms (41.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT DISTINCT(L1.num) AS ConsecutiveNums 
+FROM Logs AS L1, Logs AS L2, Logs AS L3
+WHERE L1.num = L2.num AND L2.num = L3.num AND L1.id=L2.id + 1 AND L2.id = L3.id + 1 ##condition for consecutive entries when and id column is available
