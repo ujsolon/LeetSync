7a9df9568eabe760826e58e38b09f69b4c0d0ca9
ujsolon <60507757+ujsolon@users.noreply.github.com>
2023-06-24 10:55:22 +0800

    Time: 765 ms (38.94%) | Memory: 0B (100.00%) - LeetSync

---
+++ exchange-seats/exchange-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+##skipping this one as the discussions point out to erroneous problem setup
+SELECT CASE
+           WHEN s.id % 2 <> 0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id
+           WHEN s.id % 2 = 0 THEN s.id - 1
+           ELSE
+               s.id + 1
+           END AS id,
+       student
+FROM Seat AS s
+ORDER BY id
