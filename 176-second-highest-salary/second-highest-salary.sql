f51fd0b9a9f1b531e914bf55489df76ea5e8031c
ujsolon <60507757+ujsolon@users.noreply.github.com>
2023-10-03 18:53:54 +0800

    Time: 427 ms (89.19%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,6 @@
 SELECT IFNULL(
   (SELECT salary FROM (
-  SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-  FROM Employee ) ranked
-  WHERE rnk = 2
+  SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk ##added dense_rank, sort first
+  FROM Employee ) ranked ##added a temp table name
+  WHERE rnk = 2 ##here the 2nd ranked is filtered
   LIMIT 1), Null) as SecondHighestSalary 
