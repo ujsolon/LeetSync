0e848f25cd6f6823aee38251f7e550c18b57942c
ujsolon <60507757+ujsolon@users.noreply.github.com>
2023-10-05 12:10:55 +0800

    Time: 759 ms (98.36%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -3,5 +3,5 @@
 SELECT player_id, event_date AS first_login
 FROM 
 (SELECT player_id, event_date, RANK() OVER (PARTITION BY player_id ORDER BY event_date ASC) AS rnk
-FROM Activity) ranking
+FROM Activity) ranking ##just need a temp table name so append ranking here
 WHERE rnk = 1
